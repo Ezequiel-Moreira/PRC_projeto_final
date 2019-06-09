52fee53d68b9d17ecd8e60f421bc54af1ce2a0e5
Ezequiel-Moreira <99ezequiel90@gmail.com>
2019-06-09 17:22:49 +0100

    updated rules for my own sanity

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,19 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-tabs': 'off',
+    'indent': 'off',
+    'no-multiple-empty-lines':'off',
+    'space-before-blocks':'off',
+    'space-before-function-paren':'off',
+    'key-spacing':'off',
+    'object-curly-spacing':'off',
+    'quotes':'off',
+    'keyword-spacing':'off',
+    'arrow-spacing':'off',
+    'comma-spacing':'off',
+    'no-trailing-spaces':'off'
   },
   parserOptions: {
     parser: 'babel-eslint'
