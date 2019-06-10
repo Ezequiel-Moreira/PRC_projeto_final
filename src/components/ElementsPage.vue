bdf84ae5362f722ba8bbd7bb54188f0d010ce545
Ezequiel-Moreira <99ezequiel90@gmail.com>
2019-06-10 15:55:26 +0100

    started work in elements listing

--- src/components/ElementsPage.vue
+++ src/components/ElementsPage.vue
@@ -1,12 +1,76 @@
 <template>
 	<v-container >
-    Elements page beeing redisigned, please await warmly
+    <v-data-table
+      :headers="headers"
+      :items="elements"
+      class="elevation-1"
+    >
+
+      <template v-slot:no-data>
+        <v-alert :value="true" icon="fas fa-circle-notch fa-spin">
+          Retreving data from ontology
+        </v-alert>
+      </template>
+
+      <template v-slot:items="props">
+        <tr @click="rowClicked(props.item)">
+          <td class="subheading">{{ props.item.symbol }}</td>
+          <td class="subheading">{{ props.item.name }}</td>
+          <td class="subheading">{{ props.item.atm_number }}</td>
+        </tr>
+      </template>
+
+    </v-data-table>
   </v-container>
 </template>
 
 
 <script>
+  import axios from 'axios'
+  const ontologyLink = "http://http://localhost:7200/repositories/"
+
+  var query = "PREFIX : <http://www.semanticweb.org/iamtruth/ontologies/2019/4/final_project#>\n" +
+              "select * where {\n" + 
+              "?s a :Element .\n" +
+              "}"
+  
+  var query2 = "PREFIX : <http://www.semanticweb.org/iamtruth/ontologies/2019/4/final_project#>\n" +
+                "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+                "PREFIX owl: <http://www.w3.org/2002/07/owl#>\n" +
+                "select distinct * where {\n" 
+  var query3 = " :elemental_symbol ?symbol;\n " +
+                 " :elemental_name ?name;\n" +
+                 " :elemental_atomic_number_Z ?atm_number;\n" +
+                 "}"
+
+
+  var encoded1 = encodeURIComponent(query)
+
 	export default {
+    data:() =>({
+      headers:[
+        {text:'Symbol',align:'left',sortable:true,value:'symbol',class:'title'},
+        {text:'Name',align:'left',sortable:true,value:'name',class:'title'},
+        {text:'Atomic Number',align:'left',sortable:false,value:'atm_number',class:'title'}
+      ],
+      elementsList: [],
+      elements:[]
+    }),
+    mounted: async function() {
+      try {
+        var response = await axios.get(ontologyLink + '?query=' + encoded1)
+        this.elementsList = response.data
+        for(var element in this.elementsList){
+          var fullquery2 = query2 + element + query3
+          var encoded2 = encodeURIComponent(fullquery2)
+          var elemResp = await axios.get(ontologyLink + '?query=' + encoded2)
+          var data = elemResp.data
+          this.elements.append(data)
+        }
+      } catch (error) {
+        return error
+      }
+    },
     methods: {}
 	}
 </script>
--- src/components/MoleculesPage.vue
+++ src/components/MoleculesPage.vue
@@ -6,7 +6,20 @@
 
 
 <script>
+  import axios from 'axios'
+  const ontologyLink = "http://http://localhost:7200/repositories"
+
+  var query = ""
+
 	export default {
-    methods: {}
+    methods: {},
+    mounted: async function() {
+      try {
+        var response = await axios.get(ontologyLink + '/filmes')
+        this.filmes = response.data
+      } catch (error) {
+        return error
+      }
+    }
 	}
 </script>
